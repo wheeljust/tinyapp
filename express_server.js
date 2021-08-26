1f6890dfe904d215cecf7639549fc8055df6f260
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-26 04:08:30 +0000

    formatting and comment updates, removed the early testing route handlers

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// Set up packages and modules
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
@@ -7,18 +8,17 @@ app.set("view engine", "ejs");
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
+// Database placeholder for URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
+// Express server - http requests/responses
 
-// app.get("/urls.json", (req, res) => {
-//   res.json(urlDatabase);
-// });
+app.get("/", (req, res) => {
+  res.send("Home Page Not Defined Yet");
+});
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -37,9 +37,6 @@ app.get("/urls/:shortURL", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
-// app.get("/hello", (req, res) => {
-//   res.send("<html><body>Hello <b>World</b></body></html>\n");
-// });
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
