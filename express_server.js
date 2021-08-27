469034d69e51a19d8cbe63b2abd2785ed1bba500
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-27 22:29:36 +0000

    add post route to assign username cookie

--- express_server.js
+++ express_server.js
@@ -84,6 +84,13 @@ app.post("/urls/:id", (req, res) => {
   res.redirect(`/urls`);
 });
 
+// route for login feature
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  res.cookie(username, 'username');
+  res.redirect("/urls");
+});
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
